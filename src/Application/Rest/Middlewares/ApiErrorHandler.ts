0f7fecb278c6ed5178841283a2249f9fdc8a8987
Michal <michal8989@wp.pl>
2022-10-03 23:16:52 +0200

    fix(001): Error handling fix

--- src/Application/Rest/Middlewares/ApiErrorHandler.ts
+++ src/Application/Rest/Middlewares/ApiErrorHandler.ts
@@ -2,8 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 export const apiErrorHandler = (error: any, reqest: Request, response: Response, next: NextFunction) => {
     response
-    .setHeader('Content-Type', 'application/json')
     .status(500)
-    .send({ error: error?.message || JSON.stringify(error) });
+    .json({ error: error?.message || JSON.stringify(error) });
     next(error);
   }
